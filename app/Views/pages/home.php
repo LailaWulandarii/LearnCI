b1f3f07aa9671c527023b3105d15a213626a019f
LailaWulandarii <E41220418@student.polije.ac.id>
2025-07-22 14:24:35 +0700

    add basic views and admin views (trial)

---
+++ app/Views/pages/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page Eventku</title>
+</head>
+
+<body>
+    <p>ini coba view landing page event</p>
+</body>
+
+</html>
